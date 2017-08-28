7fcf08754a17db24af0797cf9c9304f7b5d7c35a
Jeff Crouse <jeff@jeffish.com>
2017-08-28 18:13:50 -0400

    making progress with onboarding form.

---
+++ CountdownTimer.js
@@ -0,0 +1,50 @@
+const util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var CountdownTimer = function() {
+	
+	var self = this;
+	var start_time = null;
+	var countdown_from = 120000;
+
+
+	this.begin = function() {
+		start_time = Date.now();
+		setTimeout(() => {
+			start_time = null;
+			self.emit("done");
+		}, countdown_from);
+	}
+
+	this.get_time = function() {
+		if(start_time) {
+			var elapsed = Date.now() - start_time;
+			return msToTime(countdown_from - elapsed);
+		} else {
+			return msToTime(0);
+		}
+	}
+
+	// var paddy = function(n, p, c) {
+	//     var pad_char = typeof c !== 'undefined' ? c : '0';
+	//     var pad = new Array(1 + p).join(pad_char);
+	//     return (pad + n).slice(-pad.length);
+	// }
+
+	var msToTime = function(duration) {
+	    var milliseconds = parseInt((duration%1000)/100)
+	        , seconds = parseInt((duration/1000)%60)
+	        , minutes = parseInt((duration/(1000*60))%60)
+	        , hours = parseInt((duration/(1000*60*60))%24);
+
+	    hours = (hours < 10) ? "0" + hours : hours;
+	    minutes = (minutes < 10) ? "0" + minutes : minutes;
+	    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+	    return minutes + ":" + seconds;
+	}
+}
+
+util.inherits(CountdownTimer, EventEmitter);
+
+module.exports = CountdownTimer;
--- app.js
+++ app.js
@@ -6,12 +6,47 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
-//var Story = require('./models/Story');
+var socketio = require('socket.io')
+var CountdownTimer = require('./CountdownTimer')
 
 
-// var promise = mongoose.connect('mongodb://localhost/nola300', {
-// 	useMongoClient: true
-// });
+
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('./client_secret.json');
+var doc = new GoogleSpreadsheet('1-8qrqjGRQ6O8kGfZ-gFILUmwIlSU5f--mX-v3wViHrc');
+var places = {};
+var items = {}
+var themes = {};
+
+var make_key = function(name) {
+	return name.toLowerCase().replace(/ /g,"_");
+}
+
+doc.useServiceAccountAuth(creds, function (err) {
+	doc.getRows(1, function (err, rows) {
+
+		rows.forEach((row) => {
+			var key = make_key(row.name);
+			places[key] = row.name;
+		});
+
+		console.log("places", places);
+	});
+	doc.getRows(2, function (err, rows) {
+		rows.forEach((row) => {
+			var key = make_key(row.name);
+			items[key] = row.name;
+		});
+		console.log("items", items);
+	});
+	doc.getRows(3, function (err, rows) {
+		rows.forEach((row) => {
+			var key = make_key(row.name);
+			themes[key] = row.name;
+		});
+		console.log("themes", themes);
+	});
+});
 
 
 var app = express();
@@ -19,6 +54,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('view options', { layout: 'layout' });
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
@@ -29,16 +65,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', function(req, res, next) {
+
+app.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
-app.use('/onboard', function(req, res, next) {
-	res.render('onboard', { title: 'Nola300 Onboarding' });
+app.get('/onboard', function(req, res, next) {
+
+	var data = {
+		layout: false,
+		places: places,
+		items: items,
+		themes: themes,
+		title: 'Nola300 Onboarding'
+	}
+	res.render('onboard', data);
 });
 
-app.use('/booth', function(req, res, next) {
-	res.render('booth', { title: 'Nola300 Booth Interface' });
+app.post('/onboard', function(req, res, next){
+	
+	
+});
+
+app.get('/booth', function(req, res, next) {
+	res.render('booth', { layout: false, title: 'Nola300 Booth Interface' });
 });
 
 // catch 404 and forward to error handler
@@ -59,4 +109,36 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
+
+app.timer = new CountdownTimer();
+app.timer.on("done", () => {
+	console.log("TIMER DONE!");
+});
+
+
+
+
+var io = socketio();
+var boothns = io.of('/booth');
+
+// socket.io events
+boothns.on( "connection", function( socket ) {
+	console.log("booth client joined")
+	app.timer.begin();
+});
+
+setInterval(() => {
+	boothns.emit('time', app.timer.get_time())
+}, 100);
+
+
+
+var onbns = io.of('/onboard');
+onbns.on( "connection", function( socket ) {
+   console.log("onboard client joined")
+});
+
+
+
+app.io = io;
 module.exports = app;
